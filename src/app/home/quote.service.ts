0135f942f51d59276d84c43cfd3c3c8c1ea28e2d
Hannu Honka <hannu.honka@tieto.com>
2018-05-04 09:35:11 +0300

    logging

--- src/app/home/quote.service.ts
+++ src/app/home/quote.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { map, catchError } from 'rxjs/operators';
+import { Logger } from '@app/core';
+
+const log = new Logger('QuoteService');
 
 const routes = {
   quote: (c: RandomQuoteContext) => `/jokes/random?category=${c.category}`
@@ -19,6 +22,7 @@ export class QuoteService {
   constructor(private httpClient: HttpClient) { }
 
   getRandomQuote(context: RandomQuoteContext): Observable<string> {
+    log.debug('getRandomQuote');
     return this.httpClient
       .cache()
       .get(routes.quote(context))

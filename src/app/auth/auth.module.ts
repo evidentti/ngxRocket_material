c6e324e177a3b4a46a9be21828d8037d67130a13
Hannu Honka <hannu.honka@tieto.com>
2018-05-07 20:47:43 +0300

    auth module refactored

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { CoreModule } from '@app/core';
 import { SharedModule } from '@app/shared';
-import { MaterialModule } from '@app/material.module';
-import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { AuthService } from './auth.service';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule,
-    TranslateModule,
-    CoreModule,
     SharedModule,
-    FlexLayoutModule,
-    MaterialModule,
-    AuthRoutingModule,
     RouterModule
   ],
   declarations: [
-    AuthComponent
+
   ],
   providers: [
     AuthService
